c28925a25cfb6ebeb6a85d9c39284eba4b258263
aem278 <aem278@cornell.edu>
2019-03-01 12:13:48 -0500

    Fix dependency issues.
    Bot can now login and be interacted with on the server.
    Change bot presence on login.

--- src/main/java/camiscollegecorner/Main.java
+++ src/main/java/camiscollegecorner/Main.java
@@ -3,6 +3,8 @@ package camiscollegecorner;
 import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.api.events.EventDispatcher;
+import sx.blah.discord.handle.obj.ActivityType;
+import sx.blah.discord.handle.obj.StatusType;
 import sx.blah.discord.util.DiscordException;
 
 public class Main {
@@ -11,15 +13,21 @@ public class Main {
     public static final String COMMAND_LIST = "src/main/resources/commands.txt";
     public static final String CMD_PREFIX = "!";
 
-    private static final String PLAYING_TEXT = "Type !help for command list";
-    private static final String TOKEN = "NTUwODc0NjQxMjc5NzQ2MDQ4.D1pDBw.lfCdrpiXl75D8bTL_n4JXJBekNk";
+    private static final String PLAYING_TEXT = "Type " + CMD_PREFIX + "help for command list";
+    private static final String TOKEN = "NTUwODc0NjQxMjc5NzQ2MDQ4.D1rWnA.PvycXxV684ZlaZo68j2wg2krFVo";
 
     public static void main(String[] args) {
         client = buildClient(TOKEN);
-        client.changePlayingText(PLAYING_TEXT);
 
         EventDispatcher dispatcher = client.getDispatcher();
         dispatcher.registerListener(new MessageListener());
+
+        //wait until client is ready, then change its presence
+        while(!(client.isLoggedIn() && client.isReady())) {
+            //do nothing
+        }
+
+        client.changePresence(StatusType.ONLINE, ActivityType.PLAYING, PLAYING_TEXT);
     }
 
     public static IDiscordClient buildClient(String token) {
