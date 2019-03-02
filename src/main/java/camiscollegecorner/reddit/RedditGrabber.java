6d812ed080573cbed1187015db04b12660fa6a1a
aem278 <aem278@cornell.edu>
2019-03-01 20:17:42 -0500

    Create RedditGrabber interface

---
+++ src/main/java/camiscollegecorner/reddit/RedditGrabber.java
@@ -0,0 +1,12 @@
+package camiscollegecorner.reddit;
+
+/** This is an interface specifying the functionality of a class that pulls trending images from subreddits. */
+public interface RedditGrabber {
+	/**
+	 * Pulls a random image from the specified subreddit.
+	 *
+	 * @param subreddit The subreddit to pull a trending image from
+	 * @return The direct link to the image file
+	 */
+	String randomImage(String subreddit);
+}
