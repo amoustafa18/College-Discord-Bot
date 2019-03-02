d7f529943cca6dd50684062da5cbc321240f5e4b
waffleme3 <wafflesderply@gmail.com>
2019-03-01 23:41:04 -0500

    Added to the RedditImpl

--- src/main/java/camiscollegecorner/reddit/RedditGrabber.java
+++ src/main/java/camiscollegecorner/reddit/RedditGrabber.java
@@ -1,5 +1,8 @@
 package camiscollegecorner.reddit;
 
+import net.dean.jraw.models.Submission;
+import net.dean.jraw.pagination.DefaultPaginator;
+
 /** This is an interface specifying the functionality of a class that pulls trending images from subreddits. */
 public interface RedditGrabber {
 	/**
@@ -8,5 +11,5 @@ public interface RedditGrabber {
 	 * @param subreddit The subreddit to pull a trending image from
 	 * @return The direct link to the image file
 	 */
-	String randomImage(String[] subreddit);
+	String randomImage(DefaultPaginator<Submission> subreddit);
 }
--- src/main/java/camiscollegecorner/reddit/RedditImpl.java
+++ src/main/java/camiscollegecorner/reddit/RedditImpl.java
@@ -3,12 +3,17 @@ package camiscollegecorner.reddit;
 import net.dean.jraw.http.NetworkAdapter;
 import net.dean.jraw.http.OkHttpNetworkAdapter;
 import net.dean.jraw.http.UserAgent;
+import net.dean.jraw.models.Submission;
 import net.dean.jraw.models.Subreddit;
 import net.dean.jraw.RedditClient;
 import net.dean.jraw.models.EmbeddedMedia.OEmbed;
 import net.dean.jraw.models.SubmissionPreview;
 import net.dean.jraw.oauth.Credentials;
 import net.dean.jraw.oauth.OAuthHelper;
+import net.dean.jraw.pagination.DefaultPaginator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RedditImpl implements RedditGrabber {
 
@@ -21,8 +26,17 @@ public class RedditImpl implements RedditGrabber {
     NetworkAdapter adapter = new OkHttpNetworkAdapter(userAgent);
 
     RedditClient reddit = OAuthHelper.automatic(adapter, credentials);
+    //this is from the website and i have no idea why it breaks with .build and .next
+    DefaultPaginator<Submission> subreddits = reddit.subreddits().build();
 
-    public String randomImage(String[] subreddit) {
-        return "randomImage currently unimplemented";
+
+
+    public String randomImage(DefaultPaginator<Submission> subreddit) {
+        List<String> images = new ArrayList<String>();
+        for (Submission s : images.next())
+            if (!s.isSelfPost() && s.getUrl().contains("i.imgur.com")) {
+                images.add(s.getUrl());
+            }
+        return (images.get(0));
     }
 }
