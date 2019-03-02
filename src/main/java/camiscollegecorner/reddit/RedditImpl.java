740abaec2bd5e93bad05ba544e8cc22a1fdde7e8
Waffles <waffleme3>
2019-03-01 21:29:38 -0500

    fixed misplaced array

--- src/main/java/camiscollegecorner/reddit/RedditImpl.java
+++ src/main/java/camiscollegecorner/reddit/RedditImpl.java
@@ -5,7 +5,7 @@ public class RedditImpl implements RedditGrabber{
     private String testing = "this is an implement tes ";
 
 
-    public String randomImage(String subreddit[]) {
+    public String randomImage(String[] subreddit) {
         return null;
     }
 }
